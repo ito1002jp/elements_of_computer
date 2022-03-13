d465842e227c295b554d1abc41d293b378e22b2c
Ohkura Tessyuu <ito1002jp@gmail.com>
2022-03-13 13:59:28 +0900

    03 - 順序回路

--- projects/01/test.php
+++ projects/01/test.php
@@ -3,7 +3,7 @@
 
 for ($i = 0; $i < 16; $i++) {
 	$j = $i-1;
-	echo "FullAdder(a=a[${i}], b=b[${i}], c=c${j}, sum=out[${i}], carray=c${i});". "\n";
+	echo "RAM8(in=in, load=i{$i}, address=address[3..5], out=o{$i});". "\n";
 }
 
 ?>
