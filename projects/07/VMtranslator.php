522031e00b4aa54c379c6df3f4d71f40b0df619b
Ohkura Tessyuu <ito1002jp@gmail.com>
2022-04-09 22:20:19 +0900

    07 - virtual machine - StackTest

--- projects/07/VMtranslator.php
+++ projects/07/VMtranslator.php
@@ -6,16 +6,19 @@ $filePath = $argv[1];
 $parser = new Parser(fopen($filePath, 'r'));
 $wFilePath = str_replace(".vm", ".asm", $filePath);
 $writer = new CodeWriter(fopen($wFilePath, 'w'));
+$id = 0;
 while ($parser->hasMoreCommands()) {
 	$parser->advance();
 	$commandType = $parser->commandType();
 	switch($commandType) {
 		case "C_PUSH":
 		case "C_POP":
-			$writer->writePushPop($commandType, $parser->arg1(), $parser->arg2());
+			$writer->writePushPop($id, $commandType, $parser->arg1(), $parser->arg2());
+			$id++;
 			break;
 		case "C_ARITHMETIC":
-			$writer->writeArithmetic($parser->arg1());
+			$writer->writeArithmetic($id, $parser->arg1());
+			$id++;
 			break;
 	}
 }
