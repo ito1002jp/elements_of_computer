223d243f416199046f6bc5478e70e33a5f74dae2
Ohkura Tessyuu <ito1002jp@gmail.com>
2022-04-19 21:47:42 +0900

    08 - virtual machine#2 - FibonacciElement#1

--- projects/08/VMtranslator.php
+++ projects/08/VMtranslator.php
@@ -3,39 +3,51 @@ require("Parser.php");
 require("CodeWriter.php");
 
 $filePath = $argv[1];
-$parser = new Parser(fopen($filePath, 'r'));
-$wFilePath = str_replace(".vm", ".asm", $filePath);
-$writer = new CodeWriter(fopen($wFilePath, 'w'));
-$id = 0;
-while ($parser->hasMoreCommands()) {
-	$parser->advance();
-	$commandType = $parser->commandType();
-	switch($commandType) {
-		case "C_PUSH":
-		case "C_POP":
-			$writer->writePushPop($id, $commandType, $parser->arg1(), $parser->arg2());
-			$id++;
-			break;
-		case "C_ARITHMETIC":
-			$writer->writeArithmetic($id, $parser->arg1());
-			$id++;
-			break;
-		case "C_LABEL":
-			$writer->writeLabel($parser->arg1());
-			break;
-		case "C_IF":
-			$writer->writeIf($parser->arg1());
-			break;
-		case "C_GOTO":
-			$writer->writeGoto($parser->arg1());
-			break;
-		case "C_FUNCTION":
-			$writer->writeFunction($parser->arg1(),$parser->arg2());
-			break;
-		case "C_RETURN":
-			$writer->writeReturn();
-			break;
 
+$fileDirectory = opendir($filePath);
+$directoryName = pathinfo($filePath)["basename"];
+$vmFileList = [];
+while($fileName = readdir($fileDirectory)) {
+	if ($fileName != '.' && $fileName != '..' && preg_match('/\.(vm)$/i', $fileName)) {
+		$vmFileList[] = $fileName;
+	  }
+}
+closedir($fileDirectory);
+
+$wFilePath = $filePath.$directoryName.".asm";
+$writer = new CodeWriter(fopen($wFilePath, 'w'));
+foreach ($vmFileList as $vmFile) {
+	$id = 0;
+	$parser = new Parser(fopen($filePath.$vmFile, 'r'));
+	while ($parser->hasMoreCommands()) {
+		$parser->advance();
+		$commandType = $parser->commandType();
+		switch($commandType) {
+			case "C_PUSH":
+			case "C_POP":
+				$writer->writePushPop($id, $commandType, $parser->arg1(), $parser->arg2());
+				$id++;
+				break;
+			case "C_ARITHMETIC":
+				$writer->writeArithmetic($id, $parser->arg1());
+				$id++;
+				break;
+			case "C_LABEL":
+				$writer->writeLabel($parser->arg1());
+				break;
+			case "C_IF":
+				$writer->writeIf($parser->arg1());
+				break;
+			case "C_GOTO":
+				$writer->writeGoto($parser->arg1());
+				break;
+			case "C_FUNCTION":
+				$writer->writeFunction($parser->arg1(),$parser->arg2());
+				break;
+			case "C_RETURN":
+				$writer->writeReturn();
+				break;
+		}
 	}
 }
 
